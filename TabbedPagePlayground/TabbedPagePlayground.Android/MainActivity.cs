a573e67380398e9c05e9464e2f2bffd801a23db9
Mikolaj Kieres <Mikolaj Kieres>
2019-03-17 18:31:48 +1100

    Adding android splash screen

--- TabbedPagePlayground/TabbedPagePlayground.Android/MainActivity.cs
+++ TabbedPagePlayground/TabbedPagePlayground.Android/MainActivity.cs
@@ -12,13 +12,15 @@ namespace TabbedPagePlayground.Droid
 {
     [Activity(Label = "TabbedPagePlayground",
               Icon = "@mipmap/icon",
-              Theme = "@style/MainTheme",
+              Theme = "@style/Theme.Splash",
               MainLauncher = true,
               ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            SetTheme(Resource.Style.MainTheme);
+
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
